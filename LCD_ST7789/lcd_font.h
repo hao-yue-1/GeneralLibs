b8c0df249661199023a12190e80b9c76a04e78a9
hao-yue-1 <1801134420@qq.com>
2023-12-08 23:01:58 +0800

    add LCd

---
+++ LCD_ST7789/lcd_font.h
@@ -0,0 +1,16 @@
+//
+// Created by yue on 2023/12/8.
+//
+
+#ifndef _LCDFONT_H
+#define _LCDFONT_H
+
+#include <stdint.h>
+
+extern const uint8_t asc2_1608[1520];
+
+extern const unsigned char gImage_dunk[36450];  // 135*135
+
+extern const unsigned char gImage_hr[36458];    // 135*135
+
+#endif
