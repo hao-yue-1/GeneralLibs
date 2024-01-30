749f7eaae79093cda4e0781875bddb198196732b
hao-yue-1 <1801134420@qq.com>
2024-01-30 20:34:36 +0800

    修改一下格式

--- OLED_0.96/oled.h
+++ OLED_0.96/oled.h
@@ -2,12 +2,12 @@
 // Created by yue on 2023/12/5.
 //
 
-#ifndef FOC_V1_SC60228_OLED_H
-#define FOC_V1_SC60228_OLED_H
+#ifndef _OLED_H
+#define _OLED_H
 
 void OLED_Init(void);
 void OLED_Clear(unsigned char color);
 void OLED_PrintPicture(const unsigned char *ptr_pic);
 void OLED_PrintStr(unsigned char x, unsigned char y, unsigned char* data, unsigned char text_size);
 
-#endif //FOC_V1_SC60228_OLED_H
+#endif //_OLED_H
--- OLED_0.96/oled_font.h
+++ OLED_0.96/oled_font.h
@@ -2,11 +2,11 @@
 // Created by yue on 2023/12/5.
 //
 
-#ifndef FOC_V1_SC60228_OLED_FONT_H
-#define FOC_V1_SC60228_OLED_FONT_H
+#ifndef _OLED_FONT_H
+#define _OLED_FONT_H
 
 extern const unsigned char F6x8[][6];
 extern const unsigned char F8X16[];
 extern const unsigned char picture_CurryLogo[];
 
-#endif //FOC_V1_SC60228_OLED_FONT_H
+#endif //_OLED_FONT_H
